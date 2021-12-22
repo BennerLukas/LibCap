4b014cc5759ac9f5782c6d4ced3d95e4d535afd2
Phillip Lange <phillip@lange-privat.net>
2021-12-22 21:19:53 +0100

    WIP update - added page for detailed view of seats - changed internals - changed docker compose - fixed error in db

--- WebFrontend/app/views.py
+++ WebFrontend/app/views.py
@@ -6,3 +6,7 @@ import app.db_connector as dbc
 @app.route('/')  # Home
 def index():
     return render_template("/index.html")
+
+@app.route('/dashboard')
+def auslastung():
+    return render_template("/dashboard.html")
