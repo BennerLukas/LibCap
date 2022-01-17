464d9ed95d30f854eb2bda730be993bcbc21644e
Phillip Lange <phillip@lange-privat.net>
2022-01-17 12:09:41 +0100

    added mosquitto and backend to docker compose

--- src/LibCap/Backend/app/__init__.py
+++ src/LibCap/Backend/app/__init__.py
@@ -5,23 +5,23 @@ from sqlalchemy.orm import sessionmaker
 #db = SQLAlchemy(app)
 
 #connect to docker postgres db 
-#engine = create_engine('postgresql://postgres:1234@database:5432/postgres') <-- For Container
-engine = create_engine('postgresql://postgres:1234@localhost:5432/postgres')
+engine = create_engine('postgresql://postgres:1234@database:5432/postgres') # <-- For Container
+# engine = create_engine('postgresql://postgres:1234@localhost:5432/postgres')
 
 Session = sessionmaker(bind=engine)
 
 session = Session()
 
 #init db; Fill with data dump if empty
-try:
-    # test = engine.execute('SELECT * FROM CURRENT_STATUS;').fetchall()
-    # if len(test) > 0:
-    #     print("Test successful:",test)
-    # else:
-    sql_statement = open("./db/init.sql", "r").read()
-    engine.execute(sql_statement)
-except:
-    sql_statement = open("./db/init.sql", "r").read()
-    engine.execute(sql_statement)
+# try:
+#     # test = engine.execute('SELECT * FROM CURRENT_STATUS;').fetchall()
+#     # if len(test) > 0:
+#     #     print("Test successful:",test)
+#     # else:
+#     sql_statement = open("./db/init.sql", "r").read()
+#     engine.execute(sql_statement)
+# except:
+#     sql_statement = open("./db/init.sql", "r").read()
+#     engine.execute(sql_statement)
 
 from app import receiver
